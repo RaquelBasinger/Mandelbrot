2d04d10726224dcab30d1778904249b32e644130
Raquel Basinger <158781667+RaquelBasinger@users.noreply.github.com>
2024-04-24 21:31:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include "ComplexPlane.h"
 #include <iostream>
+#include "ComplexPlane.h"
+using namespace std;
+using namespace sf;
 
-int main(int argc, const char * argv[]) {
-    VideoMode::getDesktopMode().width;
-    VideoMode::getDesktopMode().height;
+int main(){
+    int pixelWidth = VideoMode::getDesktopMode().width / 2;
+    int pixelHeight = VideoMode::getDesktopMode().height / 2;
+    VideoMode vm(pixelWith,pixelHeight);
+    RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+    
+    ComplexPlane mandelbrotSet(pixelWidth, pixelHeight);
+    sf::Font font;
+    sf::Text text;
+    if(!font.loadFromFile("Freedom-10eM.ttf")){
+        cout << "Error! Could not load font" << endl;
+    }
+    
+    while(window.isOpen()){
+        Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed) {window.close();}
+            if(event.type == sf::Event::MouseButtonPressed){
+                if(event.mouseButton.button == sf::Mouse::Right){
+                    mandelbrotSet.zoomOut();
+                    mandelbrotSet.setCenter({event.mouseButton.x, event.mouseButton.y});
+                }
+                else if(event.mouseButton.button == sf::Mouse::Left){
+                    mandelbrotSet.zoomIn();
+                    mandelbrotSet.setCenter({event.mouseButton.x, event.mouseButton.y});
+                }
+            }
+            if (event.type == sf::Event::MouseMoved){
+                mandelbrotSet.setMouseLocation({ event.mouseButton.x, event.mouseButton.y });
+            }
+            if(Keyboard::isKeyPressed(Keyboard::Escape)){
+                window.close();
+            }
+            mandelbrotSet.updateRender();
+            mandelbrotSet.loadText(text);
+            window.clear();
+            window.draw(mandelbrotSet);
+            window.draw(text);
+            window.display();
+        }
+    }
+    return 0;
 }
